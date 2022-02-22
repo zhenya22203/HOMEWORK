6dff74273d04d500ed01cc1869fee6175023bad3
zhenya22203 <zenitsu005@gmail.com>
2022-02-22 15:14:47 +0600

    pablo

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+
+let some = prompt ("укажите ваш год рождения");
+let year = 2022
+let age = year - some
+ if  ( some > year ){
+     alert ("вы еще не родились") 
+    }
+  else if ( age < 30 ){
+      alert ("вы молодой")
+  }
+  else if ( age > 60 ){
+      alert ("вам стоит посетить доктора")
+  }
+else if( age < 60) {
+   alert ("у вас кризис среднего восзраста") 
+}
+
+
+
